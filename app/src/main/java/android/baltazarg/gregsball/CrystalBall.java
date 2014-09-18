507b6d72cdf7dca6d0cfd21113d936a4d960e392
Gregory Baltazar Avalos <sillygoose259@gmail.com>
2014-09-18 13:48:02 -0700

    after vid 19

--- app/src/main/java/android/baltazarg/gregsball/CrystalBall.java
+++ app/src/main/java/android/baltazarg/gregsball/CrystalBall.java
@@ -1,6 +1,7 @@
 package android.baltazarg.gregsball;
 
 import android.app.Activity;
+import android.content.Context;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -35,6 +36,9 @@ public class CrystalBall extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crystal_ball);
+
+        sensorManager = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
 
         answerText = (TextView) findViewById(R.id.answerText);
         answerText.setText(Predictions.get().getPrediction());
